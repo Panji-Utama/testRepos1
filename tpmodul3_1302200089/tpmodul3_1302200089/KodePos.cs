e06a96aab78a5089cfd149d77e928e458b78610e
Firman Panji <fpu2468@gmail.com>
2022-03-11 01:34:34 +0700

    menambahkan implementasi dengan table-driven

---
+++ tpmodul3_1302200089/tpmodul3_1302200089/KodePos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul3_1302200089
+{
+    internal class KodePos
+    {
+        private int kode;
+        private String lur;
+        public String getKodePos()
+        {
+            return lur + " " + kode;
+        }
+
+        public void setKodePos(String Lur, int Kode)
+        {
+            lur = Lur;
+            kode = Kode;
+        }
+    }
+}
--- tpmodul3_1302200089/tpmodul3_1302200089/Program.cs
+++ tpmodul3_1302200089/tpmodul3_1302200089/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul3_1302200089;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //int satu;
+        //console.write("masukkan angka: ");
+        //satu = convert.toint32(console.readline());
+        //console.writeline(satu);
+
+        Console.WriteLine("Kelurahan  Kode pos");
+
+        KodePos baru = new KodePos();
+        baru.setKodePos("Batununggal", 40266);
+        Console.WriteLine(baru.getKodePos());
+        baru.setKodePos("Kujangsari", 40287);
+        Console.WriteLine(baru.getKodePos());
+        baru.setKodePos("Mengger", 40267);
+        Console.WriteLine(baru.getKodePos());
+        baru.setKodePos("Wates", 40256);
+        Console.WriteLine(baru.getKodePos());
+        baru.setKodePos("Cijaura", 40287);
+        Console.WriteLine(baru.getKodePos());
+        baru.setKodePos("Jatisari", 40286);
+        Console.WriteLine(baru.getKodePos());
+        baru.setKodePos("Margasari", 40286);
+        Console.WriteLine(baru.getKodePos());
+        baru.setKodePos("Sekejati", 40286);
+        Console.WriteLine(baru.getKodePos());
+        baru.setKodePos("Kebonwaru", 40272);
+        Console.WriteLine(baru.getKodePos());
+        baru.setKodePos("Maleer", 40274);
+        Console.WriteLine(baru.getKodePos());
+        baru.setKodePos("Samoja", 40273);
+        Console.WriteLine(baru.getKodePos());
+
+        //Console.WriteLine("\n");
+        
+    }
+}
